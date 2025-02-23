67daa2e05293e1270ec5180b715e9cdf044db4bc
Rifat Shikder <rifat.shikder@tutors.es>
2025-02-23 12:52:36 +0600

    feat: login form

--- frontend/src/app/pages/auth/login/login.component.ts
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html'
+  imports: [
+    ReactiveFormsModule,
+    CardModule,
+    InputMaskModule,
+    PasswordModule,
+    ButtonModule,
+  ],
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
 
+  loginForm = this.fb.group({
+    phone: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  hasError = !this.loginForm.valid;
+
+  onSubmit() {
+    if (this.hasError) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
